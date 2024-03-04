3ed149fa967f41f27f3601bc363411681ac78e96
quyenpro123 <hoangvanquyenn2@gmail.com>
2024-03-04 08:04:37 +0700

    update 8h04 4/3/2024

--- README.md
+++
@@ -1,1 +0,0 @@
-# graduation_project
--- project code/testbench/downscale_tb.v
+++ project code/testbench/downscale_tb.v
@@ -1,22 +1,19 @@
 
-module max_tree_tb();
-    localparam                      data_size = 32                                              ;
+module downscale_tb();
+    localparam                      data_size = 16                                              ;
     localparam                      number_of_data = 10                                         ;
     reg                             clock_i                                                     ;
     reg                             reset_n_i                                                   ;
     reg                             start_i                                                     ;
-    reg         [data_size - 1:0]   data_i                                                      ;
+    reg  signed   [data_size - 1:0] data_i                                                      ;
     
-    wire        [data_size - 1:0]   data_max_o                                                  ;
-    wire                            max_tree_done_o                                             ;
-    
-    max_tree_block #(data_size, number_of_data) max_tree(
+    wire signed   [data_size:0]     sub_result_o                                                ;    
+    downscale_block #(data_size, number_of_data) downscale(
         .clock_i(clock_i)                                                                       ,
         .reset_n_i(reset_n_i)                                                                   ,
         .start_i(start_i)                                                                       ,
         .data_i(data_i)                                                                         ,
-        .data_max_o(data_max_o)                                                                 ,
-        .max_tree_done_o(max_tree_done_o)                                           
+        .sub_result_o(sub_result_o)
     );
     
     initial 
@@ -31,10 +28,25 @@ module max_tree_tb();
         #20 
         start_i = 1                                                                             ;
         
-    end
-    always @(posedge clock_i)
-    begin
-        data_i <= $random%20 - 10                                                               ;
+        data_i = 16'hA440                                                                       ;
+        #10 
+        data_i = 16'h2120                                                                       ;
+        #10
+        data_i = 16'hA440                                                                       ;
+        #10
+        data_i = 16'h2120                                                                       ;
+        #10
+        data_i = 16'hA440                                                                       ;
+        #10
+        data_i = 16'h2120                                                                       ;
+        #10
+        data_i = 16'hA440                                                                       ;
+        #10
+        data_i = 16'h2120                                                                       ;
+        #10
+        data_i = 16'hA440                                                                       ;
+        #10
+        data_i = 16'h2120                                                                       ;
     end
     always #5 clock_i = ~clock_i                                                                ;
 endmodule
